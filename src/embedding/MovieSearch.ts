ab61cad404f8480c844c5c0e9add47fc249855b5
Talvinder <talvindersingh1992@gmail.com>
2026-08-09 16:16:49 +0530

    movie search

---
+++ src/embedding/MovieSearch.ts
@@ -0,0 +1,60 @@
+import { generateEmbeddings, loadJSON } from "./data";
+
+function dotProduct(a: number[], b: number[]) {
+    return a.map((value, index) => value * b[index]).reduce((a, b) => a + b, 0);
+}
+
+function cosineSimilarity(a: number[], b: number[]) {
+    const product = dotProduct(a, b);
+
+    const aMagnitude = Math.sqrt(a.map(value => value * value).reduce((a, b) => a + b, 0));
+    const bMagnitude = Math.sqrt(b.map(value => value * value).reduce((a, b) => a + b, 0));
+
+    return product / (aMagnitude * bMagnitude);
+}
+
+async function main() {
+    const dataWithEmbeddings = loadJSON('MoviesDataWithEmbeddings.json');
+
+    const input = 'Super hero';
+
+    const inputEmbedding = await generateEmbeddings(input);
+
+    const similaties: {
+        title: string,
+        cast: string[],
+        similarity: number
+    }[] = [];
+
+    for (const entry of dataWithEmbeddings as any) {
+
+        if (!entry.embedding) {
+            console.log(`Skipping ${entry.input}: no embedding`);
+            continue;
+        }
+
+        const InputValue = inputEmbedding.embeddings?.[0]?.values;
+        if (!InputValue) {
+            throw new Error("Failed to generate embedding vector.");
+        }
+        const similarity = cosineSimilarity(
+            entry.embedding,
+            InputValue
+        )
+
+        similaties.push({
+            similarity,
+            title: entry.title,
+            cast: entry.cast,
+
+        })
+    }
+
+    console.log(`Similarity of ${input} with: `);
+    const sortedSimilarity = similaties.sort((a, b) => b.similarity - a.similarity).slice(0,9);
+    sortedSimilarity.forEach(similarity => {
+        console.log(`${similarity.title}: ${similarity.similarity} ${similarity.cast}`)
+    })
+}
+
+main();
---
+++ src/embedding/data.ts
@@ -0,0 +1,59 @@
+import { GoogleGenAI } from '@google/genai';
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+const googleAi = new GoogleGenAI({
+    apiKey: process.env.GEMINI_KEY,
+});
+
+export function loadJSON<T>(fileName: string): T {
+    const path = join(__dirname, fileName);
+    const rawData = readFileSync(path);
+    return JSON.parse(rawData.toString());
+}
+
+function saveDataToJSON(data: any, fileName: string) {
+    const dataString = JSON.stringify(data, null, 2);
+    const path = join(__dirname, fileName);
+    writeFileSync(path, dataString);
+    console.log(`Saved data to ${fileName}`);
+}
+
+export async function generateEmbeddings(input: string) {
+    const formattedInput = `task: movie similarity | query: ${input}`;
+
+    const response = await googleAi.models.embedContent({
+        model: 'gemini-embedding-2',
+        contents: formattedInput,
+    });
+
+    return response;
+}
+
+async function main() {
+    const data = loadJSON<any[]>('movies_list.json');
+
+    const dataWithEmbeddings = await Promise.all(
+        data.map(async (movie) => {
+
+            const text = `
+                        title: ${movie.title}
+                        genres: ${movie.genres.join(", ")}
+                        cast: ${movie.cast.join(", ")}
+                        `;
+
+            const response = await generateEmbeddings(
+                `task: sentence similarity | query: ${text}`
+            );
+
+            return {
+                ...movie,
+                embedding: response.embeddings?.[0]?.values,
+            };
+        })
+    );
+
+    saveDataToJSON(dataWithEmbeddings, 'MoviesDataWithEmbeddings.json');
+}
+
+// main();
